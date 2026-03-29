7ddf096f36926deec717bdcfe5aefdf38cad35c7
TiagoDuarte44690 <tiago.duarte44690@gmail.com>
2026-03-29 20:11:24 +0200

    version mobile

--- api/topodata.js
+++ api/topodata.js
@@ -1,5 +1,4 @@
-// api/topodata.js
 export default function handler(req, res) {
-  console.log("Handler appelé ! Méthode:", req.method);
+  console.log("Handler minimal appelé ! Méthode:", req.method);
   res.status(200).json({ voies: [], ouvreurs: [] });
 }
